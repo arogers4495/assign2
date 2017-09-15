e367ab25c52ab56a1071aeee8d5997ac047940c9
Josh <janderson152481@gmail.com>
2017-09-14 23:55:12 -0600

    New code(members class, mainframe, profilepanel) Signed-off-by: Josh <janderson152481@gmail.com>

---
+++ src/Member.java
@@ -0,0 +1,48 @@
+
+
+public class Memeber {
+
+  private String education, jobDiscription, fName, lName pic; 
+
+public Member(String pic,String edu, String job, String first, String last ){
+
+  this.pic = pic;
+  this.education = edu;
+  this.jobDiscription = job;
+  this.fname = first.
+  this.lname = last;
+
+}
+
+ public String ProfilePic(){
+
+      return pic;
+
+ }
+
+ public String FirstName(){
+
+      return fName;
+
+ }
+
+ public String LastName(){
+
+      return lName;
+
+ }
+
+ public String Education(){
+
+      return about;
+
+ }
+
+ public String Job(){
+
+      return jobDiscription;
+
+ }
+
+
+}
